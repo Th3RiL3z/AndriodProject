c9bc943790aa2a6ee3e25e4e03abe845141c3183
Th3RiL3z <kelanpr@hotmail.com>
2014-04-18 14:59:56 -0500

    this commit should have a comment in the mainactivity.java file (still testing that git is set up properly)

--- app/src/main/java/and/example/myriadchallenge/app/MainActivity.java
+++ app/src/main/java/and/example/myriadchallenge/app/MainActivity.java
@@ -1,6 +1,7 @@
 package and.example.myriadchallenge.app;
 
 //this is a comment
+//this is another comment
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
